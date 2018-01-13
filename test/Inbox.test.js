9923b89961a05a148d7b3d6fcf744555f3718df5
Mali Michael <malimccalla@gmail.com>
2018-01-13 19:07:26 +0000

    Test deployment and initial message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,20 +3,29 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const { interface, bytecode } = require('../compile');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 let coinbase;
 let inbox;
 
 beforeEach(async () => {
   coinbase = await web3.eth.getCoinbase();
+
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there'] })
     .send({ from: coinbase, gas: '1000000' });
+
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', done => {
-    console.log(inbox);
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it('it has an initial message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there');
   });
 });
